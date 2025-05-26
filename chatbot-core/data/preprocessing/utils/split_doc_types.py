df824bbd1e9d595858a1eef4f7a68dfe764080f8
Gianni <giovannivaccarino03@gmail.com>
2025-05-26 22:30:13 +0200

    Refactoring split_doc_types.

--- chatbot-core/data/preprocessing/utils/split_doc_types.py
+++ chatbot-core/data/preprocessing/utils/split_doc_types.py
@@ -2,22 +2,33 @@ import json
 from bs4 import BeautifulSoup
 import os
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-JENKINS_DOCS_PATH = os.path.join(script_dir, "../../raw/jenkins_docs.json")
-JENKINS_DOCS_PATH = os.path.abspath(JENKINS_DOCS_PATH)
-OUTPUT_PATH = os.path.join(script_dir, "../../raw/split_type_docs.json")
-
 def extract_page_content_container(soup, class_name):
-    """Extract content from the 'container' div class"""
+    """
+    Checks if a div with the given class name exists in the HTML.
+
+    Parameters:
+    - soup (BeautifulSoup): Parsed HTML document.
+    - class_name (str): Class name of the div to check.
+
+    Returns:
+    - bool: True if the div exists, False otherwise.
+    """
     content_div = soup.find("div", class_=class_name)
-    if content_div:
-        return True
-    return False
+    return bool(content_div)
 
 def split_type_docs(data):
+    """
+    Splits documentation pages into developer and non-developer types
+    based on the presence of specific container classes.
+
+    Parameters:
+    - data (dict): Dictionary where keys are URLs and values are HTML content.
+
+    Returns:
+    - tuple: (developer_urls, non_developer_urls), both as lists of URLs.
+    """
     print(f'There are {len(data)} pages')
-    with_col_lg_9 = 0
-    badUrls = []
+    not_processed_urls = []
     non_developer_urls = []
     developer_urls = []
     # Every doc page that is not in the /developer part has the content in the col-lg-9 class 
@@ -25,34 +36,17 @@ def split_type_docs(data):
         soup = BeautifulSoup(content, "html.parser")
         if extract_page_content_container(soup, "col-lg-9") == True:
             non_developer_urls.append(url)
-            with_col_lg_9 += 1
         else:
-            badUrls.append(url)
+            not_processed_urls.append(url)
     
-    print(f'With col-lg-9: {with_col_lg_9}')
-    print(len(badUrls))
+    print(f'With col-lg-9: {len(non_developer_urls)}')
 
     # Every doc page that is in the /developer part has the content in the col-8 class 
-    with_col_8 = 0
-    badUrls2 = []
-    for url in badUrls:
+    for url in not_processed_urls:
         soup = BeautifulSoup(data[url], "html.parser")
         if extract_page_content_container(soup, "col-8") == True:
             developer_urls.append(url)
-            with_col_8 += 1
-        else:
-            badUrls2.append(url)
     
-    # The result should be that len(badUrls) is 0
-    print(f'With col-8: {with_col_8}')
-    print(len(badUrls2))
+    print(f'With col-8: {len(developer_urls)}')
 
     return developer_urls, non_developer_urls
-
-    json_output = {}
-    json_output["developer"] = developer_urls
-    json_output["non_developer"] = non_developer_urls
-
-    with open(OUTPUT_PATH, "w", encoding='utf-8') as f:
-        json.dump(json_output, f, ensure_ascii=False, indent=2)
-        print("Saved correctly")
