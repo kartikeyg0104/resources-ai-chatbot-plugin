b8a3ceb4aac4c695bca2e1b60b4fe15d245a667f
Gianni <giovannivaccarino03@gmail.com>
2025-06-27 12:57:45 +0200

    Add log for test mode.

--- chatbot-core/api/config/loader.py
+++ chatbot-core/api/config/loader.py
@@ -6,6 +6,9 @@ Loads config.yml into a dictionary and exposes it as CONFIG.
 
 import os
 import yaml
+from utils import LoggerFactory
+
+logger = LoggerFactory.instance().get_logger("api")
 
 def load_config():
     """
@@ -19,6 +22,8 @@ def load_config():
         config = yaml.safe_load(f)
 
     config["is_test_mode"] = os.getenv("IS_TESTING", "0") == "1"
+    if config["is_test_mode"]:
+        logger.info("#### Running in Test Mode ####")
 
     return config
 
