f3c8ae25503a2725a808fff91a5d6e2ccac71cac
Gianni <giovannivaccarino03@gmail.com>
2025-07-05 16:47:15 +0200

    Unit tests for input component.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Chatbot } from './components/Chatbot';
 
-const footerRoot = document.getElementById('root')!;
+const footerRoot = document.getElementById('chatbot-root')!;
 
 createRoot(footerRoot).render(
   <StrictMode>
